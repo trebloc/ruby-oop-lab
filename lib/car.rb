500e5d7ea36cee7cc6426a05eb076c9fc02dc945
trebloc <ctse77@alum.berkeley.edu>
2016-01-06 14:17:15 -0800

    wrote the car and starting on race

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,13 @@
 class Car
 
   # write Car class code here
+  attr_accessor :speed  # getters and setters
 
+  def initialize
+    @speed = 0
+  end
+
+  def accelerate(mph)
+  	@speed = @speed + mph
+  end	
 end
--- lib/race.rb
+++ lib/race.rb
@@ -1,7 +1,23 @@
 require_relative "car"
 
 class Race
-
+	attr_reader :cars #getter
   # write Race class code here
+
+  	def initialize
+  		@cars = [Car.new, Car.new]
+  		random_speed = Random.new
+  		car1 = cars.first
+  		car2 = cars.last
+  		car1.accelerate(random_speed.rand(1..100))
+  		car2.accelerate(random_speed.rand(1..100))
+	end	
+
+	def winner
+	end
+
+	def loser
+	end
+	
 
 end
